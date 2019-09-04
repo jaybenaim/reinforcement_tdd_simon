e903278b13044b3d0dd2568bb599f82a49eda7fc
jaybenaim <benaimjacob@gmail.com>
2019-09-04 13:50:58 -0400

    task complete

--- simon_says.js
+++ simon_says.js
@@ -1,1 +1,34 @@
-module.exports = {echo: echo, shout: shout, repeat: repeat, startOfWord: startOfWord, firstWord: firstWord};
+const echo = string => {
+  return string;
+};
+
+const shout = string => {
+  return string.toUpperCase();
+};
+
+const repeat = (string, num) => {
+  let addSpace = string.concat(" ");
+  let newString = addSpace.repeat(num);
+  newString = newString.substring(0, newString.length - 1);
+
+  return newString;
+};
+
+const startOfWord = (string, num) => {
+  let newString = string.slice(0, num);
+  return newString;
+};
+
+const firstWord = string => {
+  let newString = string.split(" ");
+  return newString[0];
+};
+console.log(firstWord("hello world"));
+
+module.exports = {
+  echo: echo,
+  shout: shout,
+  repeat: repeat,
+  startOfWord: startOfWord,
+  firstWord: firstWord
+};
